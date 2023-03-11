3b3ef63d6141b67f54b78b49f0950b7e01aad23f
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-03-12 01:01:13 +0600

    push of two models of login and sign up

---
+++ my-atttempt/client/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from "./components/homepage/Homepage"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
+import Layout from "./components/layout/Layout";
+import {
+  BrowserRouter, 
+  Routes, 
+  Route,
+  Switch,
+  Navigate,
+  Link
+} from "react-router-dom";
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    
+  );
+}
+
+
+
+
+export default App;
+
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
