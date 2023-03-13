c1295ed760b40eae64e05636cbec2beb9a5d77ed
maria-iut1234 <mariashanta2002@gmail.com>
2023-03-14 03:04:10 +0600

    added result showing section

--- maria/lesson-structure/src/components/QuizScreen.jsx
+++ maria/lesson-structure/src/components/QuizScreen.jsx
@@ -4,16 +4,33 @@ import QuestionList from "../data/questions.json";
 import QuizResult from "./QuizResult.jsx";
 import Question from "./Question.jsx";
 
-function QuizScreen(){
+function QuizScreen({retry}){
     const [currentQuestionIndex, setCurrentQuestion] = useState(0);
     const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
     const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
+    function calculatedResult(){
+        let correct = 0;
+        QuestionList.forEach((question, index)=>{
+            if(question.correctOptionIndex == markedAnswers[index]){
+                correct++;
+            }
+        });
+        return {
+            total:QuestionList.length,
+            correct:correct,
+            percentage: Math.trunc((correct / QuestionList.length) * 100)
+        };
+    }
+    
     return (
         <div className="quiz-screen">
             {
                 isQuestionEnd ? (
-                    <QuizResult />
+                    <QuizResult
+                         return={calculatedResult()}
+                         retry={retry}
+                    />
                 ) : (
                     <Question
                         question={QuestionList[currentQuestionIndex]}
