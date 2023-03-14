6680eeb98fe28eff021104646b4f305ef211b238
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-03-15 01:41:25 +0600

    repushing landing page changes

--- client/src/landingPage/components/Home/Home.jsx
+++ client/src/landingPage/components/Home/Home.jsx
@@ -1,14 +1,13 @@
-import React from "react"
-
-import './Home.css'
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <section id="home" class="mb-28 " >
+    <section id="home" class="mb-28 ">
       <div id="hero-sec">
         <div id="hero-pictures" class="overflow-hidden">
-          <div id="box1">
-          </div>
+          <div id="box1"></div>
         </div>
 
         <div id="box2">
@@ -23,10 +22,12 @@ const Home = () => {
               Join our community of sign language learners and improve your
               communication skills with our comprehensive online courses.
             </div>
-            <button class="btn get-started float-left cursor-pointer mr-[0.7em] mt-[0.6em] h-[2.2em] text-[1.9em]">
-              Get Started
-              <i class="fas fa-arrow-circle-right"></i>
-            </button>
+            <Link to="/register" class="signupin-link ">
+              <button class="btn get-started float-left cursor-pointer mr-[0.7em] mt-[0.6em] h-[2.2em] text-[1.9em]">
+                Get Started
+                <i class="fas fa-arrow-circle-right"></i>
+              </button>
+            </Link>
           </div>
         </div>
       </div>
