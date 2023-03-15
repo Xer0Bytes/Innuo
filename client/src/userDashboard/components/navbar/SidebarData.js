e5ab9faa33e441a22594f99e91372d7229b5eb51
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-03-15 06:15:45 +0600

    added dashboard. note:fix direct to this dashboard from app.jsx

---
+++ client/src/userDashboard/components/navbar/SidebarData.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {FaCartPlus, FaEnvelopeOpenText, FaTrophy} from 'react-icons/fa';
+import {AiFillHome, AiFillSetting, AiOutlineClose} from 'react-icons/ai';
+import {IoIosPaper, IoMdPeople, IoMdHelpCircle, IoIosTrophy, IoIosLogOut} from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/dashboard',
+    icon: AiFillHome,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Lessons',
+    path: '',
+    icon: IoIosPaper,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Resources',
+    path: '',
+    icon: FaCartPlus,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Settings',
+    path: '',
+    icon: AiFillSetting,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Achievements',
+    path: '',
+    icon: IoIosTrophy,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Log out',
+    path: '/',
+    icon: IoIosLogOut,
+    cName: 'nav-text'
+  }
+];
