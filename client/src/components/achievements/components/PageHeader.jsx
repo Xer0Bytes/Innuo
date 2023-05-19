54656c3b9f779f06273da1b60883b5b709b81e58
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-20 00:30:03 +0600

    fixed animation and page header

--- client/src/components/achievements/components/PageHeader.jsx
+++ client/src/components/achievements/components/PageHeader.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import LottiePlayer from "react-lottie-player";
 
-const PageHeader = ({ title, video }) => {
-  const isVideo = typeof video === "string" && video.endsWith(".mp4");
+const PageHeader = ({
+  title,
+  lottieAnimationData,
+  isLooped,
+  animationSpeed,
+  endFrame,
+  width,
+  titleMargin,
+  titleColor
+}) => {
+  if (width == undefined) width = "w-[20em]";
+  if(titleColor == undefined) titleColor='#41CDDA';
   return (
     <div className="flex flex-col items-center justify-center mb-4">
-    <div className="w-full flex justify-center">
-    {isVideo ? (
-          <video autoPlay muted preload="auto" className="w-[220px]">
-            <source src={video} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        ) : (
-          <img src={video} className="w-[260px]" alt="Video Thumbnail" />
-        )}
+      <div className="w-full flex justify-center">
+        <LottiePlayer
+          loop={isLooped}
+          animationData={lottieAnimationData}
+          play
+          speed={animationSpeed}
+          segments={[0, endFrame]}
+          className={`${width}`}
+        />
+      </div>
+      <div
+        className={`text-6xl text-[${titleColor}]  font-bold w-full text-center text-uppercase ${titleMargin}`}
+      >
+        {title}
+      </div>
     </div>
-    <div className="text-5xl uppercase text-[#41CDDA] font-bold w-full text-center text-uppercase">{title}</div>
-  </div>
   );
 };
 
