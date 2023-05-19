83443b79cf630fff6e6da043962a5f11f7924323
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-20 00:32:58 +0600

    delete profile added

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,19 +3,22 @@ import Container from "./components/displayProfileContainer/DisplayProfileContai
 import Sidebar from '../userDashboard/components/sidebar/Sidebar'
 import Background from "./components/background/Background";
 import EditProfile from "./components/editProfile/EditProfile.jsx"
+import DeleteProfile from "./components/DeleteProfileContainer/DeleteProfileContainer";
 
 const Settings = () => {
   return (
     <span style={{background:'#edf2f7'}}>
-      <div className="">
-      <Sidebar className="" />
+      <div >
+      <Sidebar activePage={'Profile'}/>
       <div className="overflow-x-hidden flex items-center justify-center move_left">
         <main className="profile-page min-w-[98%] ">
           <Background />
           <Container />
           <EditProfile />
+          <DeleteProfile />
           {/* <ChangePassword /> */}
         </main>
+        
       </div>
       </div>
     </span>
