f95b163a91f0667499be3309c897f893d052d0b0
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-20 00:29:34 +0600

    fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,45 +6,50 @@ import Login from "./components/signUpIn/components/Login/Login";
 import EmailVerify from "./components/signUpIn/components/EmailVerify";
 import ForgotPassword from "./components/signUpIn/components/ForgotPassword/ForgotPassword";
 import PasswordReset from "./components/signUpIn/components/PasswordReset/PasswordReset";
-import Dashboard from './components/userDashboard/UserDashboard'
-import Quiz from './components/lessonStructure/Quiz'
+import Dashboard from "./components/userDashboard/UserDashboard";
+import Quiz from "./components/lessonStructure/Quiz";
 import Profile from "./components/profile/Profile.jsx";
 import Ranking from "./components/ranking/Ranking.jsx";
 import Achievements from "./components/achievements/Achievements.jsx";
-import './App.css'
+import "./App.css";
+import { Contribute } from "./components/contribute/Contribute.jsx";
 
 function App() {
   const user = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<LandingPage />}>
-          {/* <Route path="/register" element={<Signup/>} /> */}
-        </Route>
+        <Route exact path="/" element={<LandingPage />}></Route>
         <Route path="/register" element={<Signup />}></Route>
-        {/* {user && <Route path="/user-dashboard" exact element={<Main />} />} */}
 
         {user && <Route path="/userDashboard" exact element={<Dashboard />} />}
         {user && <Route path="/quiz/:lesson" exact element={<Quiz />} />}
-        {user && <Route path='/ranking' exact element= {<Ranking />} />}
-        {user && <Route path='/achievements' exact element= {<Achievements />} />}
+        {user && <Route path="/ranking" exact element={<Ranking />} />}
+        {user && (
+          <Route path="/achievements" exact element={<Achievements />} />
+        )}
+        {user && <Route path="/contribute" exact element={<Contribute />} />}
+        {user && <Route path="/profile" element={<Profile />} />}
 
+        {!user && <Route exact path="/" element={<LandingPage />}></Route>}
+        {!user && <Route path="/register" element={<Signup />}></Route>}
+        {!user && <Route path="/register" exact element={<Signup />} />}
+        {!user && <Route path="/login" exact element={<Login />} />}
+        {!user && <Route path="/" element={<Navigate replace to="/login" />} />}
+        {!user && (
+          <Route path="/EmailVerify/:email/:token" element={<EmailVerify />} />
+        )}
+        {!user && (
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        )}
+        {!user && (
+          <Route
+            path="/password-reset/:email/:token"
+            element={<PasswordReset />}
+          />
+        )}
 
-        <Route path="/register" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/EmailVerify/:email/:token" element={<EmailVerify />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="/password-reset/:email/:token"
-          element={<PasswordReset />}
-        />
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-        {/* <Route path="/password-reset" element={<PasswordReset />} /> */}
-        {/* <Route component={Error} /> */}
+        {/* <Route element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
