cea586ddd770c51d199ac809e37086d5137f3fe5
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-20 22:19:42 +0600

    fetch data in dashboard

--- client/src/components/lessonStructure/components/JoinScreen.jsx
+++ client/src/components/lessonStructure/components/JoinScreen.jsx
@@ -1,7 +1,7 @@
 import LottiePlayer from 'react-lottie-player';
 import animationData from '../assets/astronautJoinScreen.json';
 
-function JoinScreen({lesson_name, start}) {
+function JoinScreen({module_name, start}) {
   return (
     <div className="join-screen">
       <h2>Join Quiz</h2>
@@ -10,7 +10,7 @@ function JoinScreen({lesson_name, start}) {
       animationData={animationData}
       play
       style={{ width: '400px',}} />
-      <p className="lesson-name-join-screen font-bold">{lesson_name}</p>
+      <p className="lesson-name-join-screen font-bold">{module_name}</p>
       <button className="quiz_start_btn" onClick={start}>Start</button>
     </div>
   );
--- client/src/components/userDashboard/components/SubCard.jsx
+++ client/src/components/userDashboard/components/SubCard.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const SubCard = ({module_name}) => {
+const SubCard = ({module_name, module_exist}) => {
+  let link;
+
+  if (module_exist) {
+    link = `/quiz/${module_name}`;
+  } else {
+    link = '/userDashboard';
+  }
   return (
-    <div><div><div className="min-w-[20em] p-2 py-3 mt-3 bg-white border border-gray-200 rounded-lg shadow">
-    <Link to={`/quiz/${module_name}`}>
+    <div className="min-w-[20em] p-2 py-3 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+    <Link to={link}>
       <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900">
         {module_name}
       </h5>
     </Link>
     
-  </div></div></div>
+  </div>
   )
 }
 
