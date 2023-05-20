224e29b66af655a00a800c6eba9cfbf8247e9940
maria-iut1234 <mariashanta2002@gmail.com>
2023-05-20 19:34:26 +0600

    fetched data for topics and modules in lesson form

--- client/src/components/userDashboard/components/SubCard.jsx
+++ client/src/components/userDashboard/components/SubCard.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 
 const SubCard = ({module_name}) => {
   return (
-    <div><div><div className="min-w-[20em] p-2 py-3 mt-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div><div><div className="min-w-[20em] p-2 py-3 mt-3 bg-white border border-gray-200 rounded-lg shadow">
     <Link to={`/quiz/${module_name}`}>
-      <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900">
         {module_name}
       </h5>
     </Link>
