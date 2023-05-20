d9ea8431b53b14b1a555180789f9cfb35fe2a921
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-20 07:37:47 +0600

    added lesson form & css fixing

--- client/src/components/contribute/components/ModuleForm.jsx
+++ client/src/components/contribute/components/ModuleForm.jsx
@@ -1,26 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Dropdown } from "./Dropdown";
 import InputField from "./InputField";
+import { useNavigate } from "react-router-dom";
+import newRequest from "../../../utils/newRequest";
 
 const ModuleForm = () => {
-    const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     //dont worry this is just all the form value ;)
     moduleFormTopicName: "",
     moduleFormModuleID: "",
     moduleFormModuleName: "",
   });
-  const handleSubmit = (e) => {
-    event.preventDefault();
+  const [error, setError] = useState(null);
+  const handleInputChange = (field, value) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: value,
+    }));
+  };
+  const navigate=useNavigate();
+  const config_header = {
+    header: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(formData);
     // Do something with the form data
+    try {
+      const res = await newRequest.post(
+        "/module/contribute",
+        { topicTitle: formData.moduleFormTopicName, moduleID: formData.moduleFormModuleID, moduleTitle: formData.moduleFormModuleName },
+        config_header
+      );
+      console.log(res.data);
+      //localStorage.setItem("allModules", JSON.stringify(res.data));
+      navigate("/contribute");
+    } catch (err) {
+      if (err.response && err.response.data && err.response.data.message) {
+        setError(err.response.data.message);
+      } else {
+        // console.log(err);
+        setError("An error occurred during database insertion.");
+      }
+    }
     console.log(formData);
   };
+
+  const allTopics = JSON.parse(localStorage.getItem("allTopics"));
+
+  const topicTitles = allTopics.map((item) => item.topicTitle);
+  console.log(topicTitles);
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <Dropdown
           id={"select-topic-for-module"}
           label={"Select Topic"}
-          values={["Alphabets"]}
+          values={topicTitles}
           onValueChange={(value) =>
             handleInputChange("moduleFormTopicName", value)
           }
@@ -40,15 +80,15 @@ const ModuleForm = () => {
           }
         />
         <div className="w-full mr-auto ml-auto text-md text-center mt-6">
-                <button
-                  type="submit"
-                  className="savechanges_btn"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Add Module
-                </button>
-              </div>
+          <button
+            type="submit"
+            className="savechanges_btn"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            Add Module
+          </button>
+        </div>
       </form>
     </>
   );
