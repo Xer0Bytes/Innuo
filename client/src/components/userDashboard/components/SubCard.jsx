7854f4e90192f5f4ccc16b469c23af406ee8ecb7
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-21 17:36:44 +0600

    completed modules on progress

--- client/src/components/userDashboard/components/SubCard.jsx
+++ client/src/components/userDashboard/components/SubCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const SubCard = ({module_name, module_exist}) => {
+const SubCard = ({module_name, module_exist, has_questions, bg_color}) => {
   let link;
 
   if (module_exist) {
-    link = `/quiz/${module_name}`;
+    link = has_questions? `/quiz/${module_name}`: `/userDashboard`;
   } else {
     link = '/userDashboard';
   }
   return (
-    <div className="min-w-[20em] p-2 py-3 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+    <div className={`min-w-[20em] p-2 py-3 mt-3 bg-${bg_color} border border-gray-200 rounded-lg shadow`}>
     <Link to={link}>
       <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900">
         {module_name}
