29e723ec13622276fb9fb841733f54e21caf6ce9
maria-iut1234 <mariashanta2002@gmail.com>
2023-05-21 20:18:48 +0600

    func for getting all ach backend

--- server/controllers/achievement.controller.js
+++ server/controllers/achievement.controller.js
@@ -1,3 +1,13 @@
-export const fn = (req, res)=>{
-   
-}
+import Achievement from "../models/achievement.model.js";
+
+export const getAllAch = async (req, res, next) => {
+    try {
+      //all chieves
+      const ach = await Achievement.find({});
+  
+      res.status(200).send(ach);
+    } catch (err) {
+      next(err);
+      console.log(err);
+    }
+  };
