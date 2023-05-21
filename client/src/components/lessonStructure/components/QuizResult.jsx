1ea8ac0500c9ba1a507df8a9d1cb922c202e1a18
maria-iut1234 <mariashanta2002@gmail.com>
2023-05-21 18:51:30 +0600

    integrating backend, result screen exp fix

--- client/src/components/lessonStructure/components/QuizResult.jsx
+++ client/src/components/lessonStructure/components/QuizResult.jsx
@@ -1,12 +1,62 @@
 import LottiePlayer from "react-lottie-player";
 import endQuizAnimation from "../assets/astronautResultScreen.json";
+import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import newRequest from "../../../utils/newRequest";
+import getCurrentUser from "../../../utils/getCurrentUser";
 
 function QuizResult({
   userExp,
   retry,
+  previousExp
 }) {
+  //console.log(userExp);
 
-  //WRITE THE FUNCTION
+  const config_header = {
+    header: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  useEffect(() => {
+
+    const currentUser = getCurrentUser();
+
+    const setUserExp = async () => {
+      try {
+      
+        const res = await newRequest.post(
+          `/quiz/updateExp/${currentUser._id}`,
+          { updateExp: userExp-5 },
+          config_header
+        );
+    
+        localStorage.setItem("currentUser", JSON.stringify(res.data));
+
+      } catch (err) {
+        if (err.response && err.response.data && err.response.data.message) {
+          setError(err.response.data.message);
+        } else {
+          setError("An error occurred during updating EXP.");
+          //console.log(err);
+        }
+      }
+    };
+
+    setUserExp();
+  }, []);
+
+  const navigate = useNavigate();
+  const navigateToDashboard = () => {
+    const buttonOnClick = () => {
+      navigate("/userDashboard");
+    };
+  
+    const timer = setTimeout(buttonOnClick, 5000);
+    return () => clearTimeout(timer);
+  };
+  const expEarned = Number(userExp-previousExp);
+
   return (
     <div className="result-screen overflow-y-hidden	">
       <LottiePlayer
@@ -16,8 +66,8 @@ function QuizResult({
         className="w-[30em]"
       />
       <span className="-mt-[8em]">
-        <h2 className="text-2xl">You earned +{userExp} XP!</h2>
-        <button onClick={retry} className="quiz_retry_btn mt-12">
+        <h2 className="text-2xl">You earned {expEarned <0 ? "" : "+"} {expEarned} XP!</h2>
+        <button onClick={navigateToDashboard()} className="quiz_retry_btn mt-12">
           Next
         </button>
       </span>
