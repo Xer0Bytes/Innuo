94bcd479e51c779ff507af7555f9df41546a0bfb
maria-iut1234 <mariashanta2002@gmail.com>
2023-06-24 14:18:30 +0600

    verification email upon sign up setup
    only email being sent now
    email link not being verified
    have to make html page on client side for verified

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,32 @@
+//models
 import User from "../models/user.model.js";
+import Verification from "../models/verification.model.js";
+import { sendVerificationEmail } from "../middleware/sendVerificationEmail.js";
+
+//pw hashing
 import bcrypt from "bcrypt";
+
+//env variable
+import dotenv from "dotenv";
+
+//token
 import jwt from "jsonwebtoken";
+
+//error handling
 import createError from "../utils/createError.js";
-import sendEmail from "../utils/sendEmail.js";
+
+//email handling
+import nodemailer from "nodemailer";
+
+//uuid
+import { v4 as uuidv4 } from "uuid";
+
+//path for static verified page
+import path from "path";
+
+dotenv.config();
+
+
 
 export const register = async (req, res, next) => {
   try {
@@ -10,7 +34,8 @@ export const register = async (req, res, next) => {
 
     const nameRegex = /^[a-zA-Z\s]+$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
 
     if (!passwordRegex.test(password)) {
       // Password format is invalid
@@ -35,15 +60,102 @@ export const register = async (req, res, next) => {
       password: hashedPassword,
     });
 
-    const res = await newUser.save();
+    newUser
+      .save()
+      .then((result) => {
+        //handle account verification
+        sendVerificationEmail(result, res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    //const user = await User.findOne({email: email});
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+};
 
-    // const emailToken = "84cf232a0d16867b4269b3d4c6e79f04";
-    // const verifyEmail = `http://localhost:5173/user/${user._id}/verify/${emailToken}`;
-    // const resEmail = await sendEmail(user.email, "Verify Email", verifyEmail);
+//verify email
+export const verifyEmail = async (req, res, next) => {
+  try {
+    let {userID, uniqueString} =req.params;
 
-    //res.status(201).send(resEmail);
+    Verification
+      .find({userID})
+      .then((result) => {
+        if(result.length > 0) {
+          const {expiresAt} = result[0];
+          const hashedUniqueString = result[0].hashedUniqueString;
+
+          if (expiresAt < Date.now()) {
+            //record has expired
+            Verification
+              .deleteOne({userID})
+              .then((result) => {
+                User
+                  .deleteOne({_id: userID})
+                  .then()
+                  .catch((error) => {
+                    console.log("error while deleting user record")
+                  })
+              })
+              .catch((error) => {
+                console.log("error while deleting expired user");
+              })
+
+          } else {
+            //valid record exists
+            bcrypt
+              .compare(uniqueString, hashedUniqueString)
+              .then((result) => {
+                if(result) {
+                  //strings match
+                  User
+                    .updateOne({_id: userID}, {verified: true})
+                    .then(() => {
+                      Verification
+                        .deleteOne({userID})
+                        .then(() => {
+                          res.sendFile(path.join(__dirname, "http://localhost:5173/"));
+                        })
+                        .catch((error) => {
+                          console.log(error);
+                        })
+                    })
+                    .catch((error) => {
+                      console.log("error while updating user");
+                    })
+
+                } else {
+                  console.log("invalid verification details")
+                }
+              })
+              .catch((error) => {
+                console.log("error while comparing hashed strings");
+              });
+          }
+
+        } else {
+          console.log("No record found");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  } catch(err) {
+    next(err);
+    console.log(err);
+  }
+};
+
+//verified page route
+export const redirection = async(req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, "http://localhost:5173/"));
+  
+
   } catch (err) {
     next(err);
     console.log(err);
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, verifyEmail, redirection } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register)
+router.get("/verify/:id/:unique", verifyEmail)
+router.get("/verified", redirection)
 router.post("/login", login);
 router.post("/logout", logout)
 
