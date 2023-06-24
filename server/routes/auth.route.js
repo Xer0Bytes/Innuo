e6e06bcb6a31365d825f32f02584ee38328236da
maria-iut1234 <mariashanta2002@gmail.com>
2023-06-24 23:54:07 +0600

    email sending bug fixed

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { register, login, logout, verifyEmail, redirection } from "../controllers/auth.controller.js";
+import { register, login, logout, verifyEmail } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register)
 router.get("/verify/:id/:unique", verifyEmail)
-router.get("/verified", redirection)
 router.post("/login", login);
 router.post("/logout", logout)
 
