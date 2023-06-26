571d705b49475371e9af7e7f08f0251b0ba3a1d7
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-06-27 01:14:07 +0600

    implemneted frontend of registration
    including mail sending

--- client/src/components/signUpIn/components/EmailVerify/index.jsx
+++ client/src/components/signUpIn/components/EmailVerify/index.jsx
@@ -1,43 +1,68 @@
 import { useEffect, useState, Fragment } from "react";
 import { useParams, Link } from "react-router-dom";
+import LottiePlayer from "react-lottie-player";
 import axios from "axios";
-import success from "../../assets/success.png";
 import styles from "./styles.module.css";
+import NotFound from "../NotFound/NotFound";
+import mailVerified from "../../assets/mailVerified.svg";
+import loadingAnimation from "../../assets/loadingAnimation.json";
+import newRequest from "../../../../utils/newRequest";
 
 const EmailVerify = () => {
-	const [validUrl, setValidUrl] = useState(false);
-	const param = useParams();
+  const [validUrl, setValidUrl] = useState(false);
+  const param = useParams();
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const verifyEmailUrl = async () => {
-			try {
-				const url = `http://localhost:7000/api/auth/verify/${param.id}/${param.unique}`;
-				const data = await axios.get(url);
-				console.log(data);
-				setValidUrl(true);
-			} catch (error) {
-				console.log(error);
-				setValidUrl(false);
-			}
-		};
-		verifyEmailUrl();
-	}, [param]);
+  useEffect(() => {
+    const verifyEmailUrl = async () => {
+      try {
+        // const url = `http://localhost:7000/api/auth/verify/${param.id}/${param.unique}`;
+        const res = await newRequest.get(`auth/verify/${param.id}/${param.unique}`);
+        if (res.status < 400) {
+          setValidUrl(true);
+          setLoading(false);
+        } else {
+          setValidUrl(false);
+          setLoading(false);
+        }
+      } catch (error) {
+        setLoading(false);
+        setValidUrl(false);
+        console.log(error);
+      }
+    };
+    verifyEmailUrl();
+  }, [param]);
 
-	return (
-		<Fragment>
-			{validUrl ? (
-				<div className={styles.container}>
-					<img src={success} alt="success_img" className={styles.success_img} />
-					<h1>Email verified successfully</h1>
-					<Link to="/login">
-						<button className={styles.green_btn}>Login</button>
-					</Link>
-				</div>
-			) : (
-				<h1>404 Not Found</h1>
-			)}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      {loading ? (
+        <LottiePlayer
+          loop={true}
+          animationData={loadingAnimation}
+          className={`w-1/2 m-auto`}
+          play
+        />
+      ) : validUrl ? (
+        <div className="w-3/4 text-center mx-auto">
+          <div className="m-auto p-2 text-center">
+            <img src={mailVerified} className="w-1/2 p-4 mx-auto" />
+            <h1 className="text-4xl font-bold">{"Email verified!"}</h1>
+            <h3 className="text-lg pt-2">
+              {
+                "Thank you, your email has been verified. Your account is now active. Click on the button below to proceed to your account!"
+              }
+            </h3>
+          </div>
+          <Link to="/login" className="w-full mx-auto text-center">
+            <button className={"signin_btn"}>Login</button>
+          </Link>
+        </div>
+      ) : (
+        <NotFound />
+      )}
+    </Fragment>
+  );
 };
 
 export default EmailVerify;
