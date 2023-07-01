fbad78fe937b7bfe11292b390827c281a73356a4
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-07-02 02:08:31 +0600

    completed module gives notification

--- client/src/components/userDashboard/components/SubCard.jsx
+++ client/src/components/userDashboard/components/SubCard.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Notification2 from "./Notification2";
+import PopUp from "../../lessonStructure/components/PopUp";
 
-const SubCard = ({ module_name, module_id, has_questions, bg_color }) => {
+const SubCard = ({
+  module_name,
+  module_id,
+  has_questions,
+  bg_color,
+  completed,
+}) => {
   const [noModuleNotification, setNoModuleNotification] = useState(false);
+  const [visibleModal, setVisibleModal] = useState(false);
+
   const navigate = useNavigate();
 
   const onClickModule = (e) => {
     e.stopPropagation();
     if (has_questions) {
-      navigate(`/quiz/${module_name}/${module_id}`);
+      if (completed === true) {
+        console.log("is completed");
+        setVisibleModal(true);
+      } else {
+        navigate(`/quiz/${module_name}/${module_id}`);
+      }
     } else {
       setNoModuleNotification(true);
       const timer = setTimeout(() => {
@@ -22,12 +36,27 @@ const SubCard = ({ module_name, module_id, has_questions, bg_color }) => {
     <div
       className={`min-w-[20em] p-2 py-3 mt-3 bg-${bg_color} border border-${bg_color} rounded-lg shadow`}
     >
+      {visibleModal && (
+        <PopUp
+          message={
+            "You have already completed this module and will not earn any experience points. Are you sure you want to continue?"
+          }
+          redButtonText={"Take me back"}
+          redButtonFunction={()=>setVisibleModal(false)}
+          normalButtonText={"Yes, Continue"}
+          normalButtonFunction={()=>navigate(`/quiz/${module_name}/${module_id}`)}
+          setModalVisibility={setVisibleModal}
+        />
+      )}
+
       <div onClick={onClickModule} className="cursor-pointer">
         <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900">
           {module_name}
         </h5>
       </div>
-      {noModuleNotification && <Notification2 text={"Hold tight! Questions coming soon."} />}
+      {noModuleNotification && (
+        <Notification2 text={"Hold tight! Questions coming soon."} />
+      )}
     </div>
   );
 };
