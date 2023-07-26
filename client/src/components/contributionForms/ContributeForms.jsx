d2bfa701c3a6fb4a9e51f27422a76900e6cafa95
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-07-26 22:33:46 +0600

    moving directory for tabs

---
+++ client/src/components/contributionForms/ContributeForms.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
+import QuestionForm from "./components/QuestionForm";
+import ModuleForm from "./components/ModuleForm";
+import TopicForm from "./components/TopicForm";
+import LessonForm from "./components/LessonForm";
+
+const ContributeForms = () => {
+  const [formType, setFormType] = useState("topic");
+  return (
+    <>
+      <div className="p-4">
+        <div className="p-4 border-2 items-center border-dashed rounded-lg border-gray-700 mt-3 w-3/4 mx-auto items-center">
+          <div className="text-6xl text-[#FA9BAF] font-bold w-full text-center text-uppercase mb-8">
+            {formType === "topic" && "Add Topic Form"}
+            {formType === "module" && "Add Module Form"}
+            {formType === "question" && "Add Question Form"}
+            {formType === "lesson" && "Add Lesson Form"}
+          </div>
+          <Dropdown
+            id={"form-type"}
+            label={"Form Type"}
+            values={["Add Topic", "Add Module", "Add Lesson", "Add Question"]}
+            setTopicValue={setFormType}
+          />
+          {/* drop down for topic/question/lesson  */}
+
+          {/* add topic form  */}
+          {formType === "topic" && <TopicForm />}
+
+          {/* add module form  */}
+          {/* FETCH TOPIC NAMES FROM DB AND PASS IT TO VALUES============== */}
+          {formType === "module" && <ModuleForm />}
+
+          {/* add Lesson form  */}
+          {formType === "lesson" && <LessonForm />}
+
+          {/* add Question form  */}
+          {formType === "question" && <QuestionForm />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContributeForms;
